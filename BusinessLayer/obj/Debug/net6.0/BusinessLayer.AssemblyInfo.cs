f0f47e0134c40d592d4b8522d07aac4febf2882c
Yugdeep Parihar <yugdeepparihar098@gmail.com>
2025-03-05 14:07:33 +0530

    [Yugdeep Parihar] ADD:Ability to save the Greeting App to save the Greeting Message in the Repository

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0902953810ac360bd5abaabb7089e16dffee94e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb50b59ffba3affcb5dc8d68f8901b445f8df4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0902953810ac360bd5abaabb7089e16dffee94e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb50b59ffba3affcb5dc8d68f8901b445f8df4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
